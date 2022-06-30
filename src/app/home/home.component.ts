edbd51f0f708eddeba4ae95d81cc1ef25c0c9582
tom-kotlar <t.kotlar@yahoo.com>
2022-06-30 13:18:39 +0100

    Ref(home.*): Logout Button & User Info

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+export interface UserInfo {
+  identityProvider: string;
+  userId: string;
+  userDetails: string;
+  userRoles: string[];
+}
 
 @Component({
   selector: 'app-home',
@@ -6,10 +12,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-
+  userInfo: UserInfo | undefined;
   constructor() { }
 
-  ngOnInit(): void {
+  async ngOnInit() {
+    this.userInfo = await this.getUserInfo();
+    console.log(this.userInfo, "USER INFO")
+  }
+
+
+  async getUserInfo() {
+    try {
+      const response = await fetch('/.auth/me');
+      const payload = await response.json();
+      const { clientPrincipal } = payload;
+      console.log(clientPrincipal, "<<<")
+      return clientPrincipal;
+    } catch (error) {
+      console.error('No profile could be found');
+      return undefined;
+    }
   }
 
 }
