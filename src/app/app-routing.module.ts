b2fd34214ce1c5653d06bba2e573b84d3415ed9f
tom-kotlar <t.kotlar@yahoo.com>
2022-07-06 16:56:06 +0100

    Reaf(app-routing): removing login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,6 @@ import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
 ];
 
