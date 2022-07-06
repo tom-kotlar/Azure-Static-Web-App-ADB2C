5c8266553898523ec3fb1e9822668b82bc11db8d
tom-kotlar <t.kotlar@yahoo.com>
2022-07-06 16:49:47 +0100

    Ref(*): Details

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,6 @@ import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  // { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
